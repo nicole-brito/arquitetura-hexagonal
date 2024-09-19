a83f2714eeaf42f0f2bd7303b478b0c10bbf48dc
nic <nicolem.britto@gmail.com>
2024-09-18 21:07:23 -0300

    first commit

---
+++ src/main/kotlin/com/nicbrt/cursohexagonal/CursoHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.nicbrt.cursohexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CursoHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<CursoHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/com/nicbrt/cursohexagonal/CursoHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicbrt.cursohexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CursoHexagonalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
